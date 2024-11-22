2c57c32e59be1c9767ef898ab4866904f491c0d7
FVidalYepz <Frananyelvidalyepez@gmail.com>
2024-11-21 21:02:26 -0400

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import colors from "tailwindcss/colors";
 
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      scrollbar: {
+        thumb: {
+          rounded: "full",
+        },
+        track: {
+          rounded: "full",
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+
+      colors: {},
+    },
+    colors: {
+      ...colors,
+    },
+  },
+  plugins: [
+    require("tailwind-scrollbar")({
+      nocompatible: true,
+      preferredStrategy: "pseudoelements",
+    }),
+  ],
+};
