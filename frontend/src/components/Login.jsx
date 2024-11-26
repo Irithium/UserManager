d6dfe393d9fc34fcc9a71b10226370d003dfa007
FVidalYepz <Frananyelvidalyepez@gmail.com>
2024-11-26 09:44:32 -0400

    Update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { login } from "../services/auth";
 import toast from "react-hot-toast";
 import { validateEmail, validatePassword } from "../utils/validation";
+import logoImage from "../assets/image/userManager.png";
 import { Link, useNavigate } from "react-router-dom";
 import ReviewsCarousel from "./Reviews";
 import { BiLogIn } from "react-icons/bi";
@@ -73,7 +74,7 @@ const Login = () => {
     <div className="content-center h-screen bg-gradient-to-br from-peach-400  to-vermilion-500">
       <div className="flex flex-col md:flex-row flex-1 justify-center items-center w-full h-full lg:h-[85%] lg:w-4/5 md:bg-black-100/90 md:rounded-lg mx-auto overflow-hidden">
         <div className="flex flex-col justify-center w-4/5 md:w-3/5 lg:w-2/5 h-auto md:h-full items-center lg:py-6 bg-white rounded-lg md:rounded-r-lg py-4">
-          <h2 className="md:w-fit px-2 text-2xl text-center mx-auto font-bold text-dim_gray-200">
+          <h2 className="md:w-fit px-2 py-4 lg:py-2 text-2xl text-center mx-auto font-bold text-dim_gray-200">
             USERMANAGER
           </h2>
           <div className="flex flex-col items-center justify-center text-dim_gray-200 h-full py-2 w-full">
@@ -97,7 +98,13 @@ const Login = () => {
             </Link>
           </div>
         </div>
-        <div className="hidden md:flex flex-col items-center justify-center w-3/5 h-4/5 ">
+        <div className="relative hidden md:flex flex-col items-center justify-center w-3/5 h-4/5 ">
+          <img
+            src={logoImage}
+            alt={logoImage}
+            className="absolute w-1/5 -top-12 -right-4"
+          />
+
           <div className="text-gray-100 text-2xl text-center w-3/4 font-semibold leading-relaxed mb-8">
             <div>
               Visualize, manage, and grow: the power of your users in one table
